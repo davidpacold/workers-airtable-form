2856f57464d9c25d479a2c2ba8d5f9ebfba039c3
David <David.Pacold@Gmail.com>
2024-07-29 11:48:01 -0400

    CORS

---
+++ worker/OG_index.js
@@ -0,0 +1,78 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+const createAirtableRecord = body => {
+  return fetch(`https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${encodeURIComponent(AIRTABLE_TABLE_NAME)}`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+      'Content-type': `application/json`
+    }
+  })
+}
+
+const submitHandler = async request => {
+  if (request.method !== "POST") {
+    return new Response("Method Not Allowed", {
+      status: 405
+    })
+  }
+  const SECRET_KEY = `${TURNSTILE_SECRET}`;  
+  const body = await request.formData();
+// Turnstile injects a token in "cf-turnstile-response".
+    const token = body.get('cf-turnstile-response');
+    const ip = request.headers.get('CF-Connecting-IP');
+
+    // Validate the token by calling the "/siteverify" API.
+    let formData = new FormData();
+    formData.append('secret', SECRET_KEY);
+    formData.append('response', token);
+    formData.append('remoteip', ip);
+
+    const result = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
+        body: formData,
+        method: 'POST',
+    });
+
+    const outcome = await result.json();
+    if (!outcome.success) {
+        return new Response('The provided Turnstile token was not valid! \n' + JSON.stringify(outcome));
+    }
+    // The Turnstile token was successfuly validated. Proceed with your application logic.
+
+  const {
+    first_name,
+    last_name,
+    email,
+    phone,
+    subject,
+    message
+  } = Object.fromEntries(body)
+
+  const reqBody = {
+    fields: {
+      "First Name": first_name,
+      "Last Name": last_name,
+      "Email": email,
+      "Phone Number": phone,
+      "Subject": subject,
+      "Message": message
+    }
+  }
+
+  await createAirtableRecord(reqBody)
+  return Response.redirect(FORM_URL)
+}
+
+async function handleRequest(request) {
+  const url = new URL(request.url)
+
+  if (url.pathname === "/submit") {
+    return submitHandler(request)
+  }
+
+  // return new Response.redirect(FORM_URL)
+  return new Response.redirect(SUBMIT_URL)
+}
